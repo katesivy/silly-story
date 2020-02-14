8611e55bd1a5c35b836a60dc20797081207bf2bb
katesivy <58646769+katesivy@users.noreply.github.com>
2020-02-14 10:17:28 -0500

    edits

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function randomValueFromArray(array){
 
 
 
-function result(newStory) {
+function result() {
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
